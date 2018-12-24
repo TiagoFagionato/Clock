e0e81304d2449b60301d15f63158783a6a950827
Tiago Fagionato <tiagofagionato@gmail.com>
2018-12-24 00:29:24 -0200

    base

---
+++ script.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+    setInterval(clock, 1000);
+}
+function clock() {
+    var now = new Date();
+    var time = now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds();
+    document.getElementById("clock").innerHTML = time;
+}
