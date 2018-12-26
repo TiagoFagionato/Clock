125c366a6847d69110c5a8530a93146e9bb04ea6
Tiago Fagionato <tiagofagionato@gmail.com>
2018-12-26 03:24:33 -0200

    Adicionamento da função 'formatarNum'

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
-window.onload = function () {
+window.onload = function() {
+	clock();
 	setInterval(clock, 1000);
+};
+
+function formatarNum(x) { //Retorna digitos com duas casas
+	for(var i = 0; i <= x.length; i++) {
+		var str = new String(x[i]);
+		if(str.length < 2) {
+			str = 0 + str;
+			x[i] = str;
+		}
+	}
+	return(x[0] + ":" + x[1] + ":" + x[2]);
 }
 
 function clock() {
 	var now = new Date();
-	var time = now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds();
-	document.getElementById("clock").innerHTML = time;
+	var x = [];
+	x.push(now.getHours(), now.getMinutes(), now.getSeconds());
+	document.getElementById("clock").innerHTML = formatarNum(x);
 }
